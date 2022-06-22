3bc9b4f732feadaa0a366ffec762198509dca4b6
Konee <104918313+KoneeVibes@users.noreply.github.com>
2022-06-21 19:51:54 -0500

    Committed local file to the remote repository

---
+++ Task1.js
@@ -0,0 +1,11 @@
+// Assignment
+
+// Write a javascript code that prints out your name, height, and country on the screen
+
+const myName = 'Ofofonono';
+var myHeight = '1.65m';
+const myCountryOfOrigin = 'Nigeria';
+
+console.log(myName);
+console.log(myHeight);
+console.log(myCountryOfOrigin);
